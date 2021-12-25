fcd841a6460b7ae8063c94bb6fed7f431335588b
Chandan Rai <chandanr@exzeo.com>
2021-12-25 13:50:52 +0530

    initial commit

---
+++ RazorClassLibary/Forecaster.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using RazorLight;
+
+namespace RazorClassLibary
+{
+    public class Forecaster
+    {
+
+
+        public async Task<string> Get()
+        {
+
+
+            // Embedded Path 
+            // Works when dependecy is reffered as package or dll reference
+            var engine = new RazorLightEngineBuilder()
+                .UseEmbeddedResourcesProject(typeof(Forecaster))
+                .UseMemoryCachingProvider()
+                .EnableDebugMode(true)
+                .Build();
+            var result = await engine.CompileRenderAsync<object>("Views.MyFeature.page", new object());
+
+
+            // File based path   
+            // Works when dependecy is reffered as project reference
+            // string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Views");
+            // var engine = new RazorLightEngineBuilder()
+            //           .UseFileSystemProject(path)
+            //           .UseMemoryCachingProvider()
+            //           .Build();
+
+            // var result = await engine.CompileRenderAsync(Path.Combine(path, "MyFeature", "page.cshtml"), new object());
+
+            return result;
+        }
+    }
+}
---
+++ RazorLightSample/Controllers/ForecastController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorClassLibary;
+
+namespace RazorLightSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ForecastController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            var result = new Forecaster().Get().GetAwaiter().GetResult();
+            return result;
+        }
+    }
+}
